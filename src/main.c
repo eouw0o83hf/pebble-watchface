35757fe74707c72dfc9c72ddac162c213f3f5279
eouw0o83hf <nathanlandis@gmail.com>
2014-12-25 15:34:16 -0600

    Actually show something in the UI

--- src/main.c
+++ src/main.c
@@ -2,15 +2,29 @@
 	
 /// The main watchface window
 static Window *s_main_window;
-	
+/// To show text on the screen
+static TextLayer *s_time_layer;
+
 /// Called when the window is loaded
 static void main_window_load(Window *window) {
+	// Be sure to _destroy this on _unload()
+	s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
 	
+	// Text/UI initialization
+	text_layer_set_background_color(s_time_layer, GColorClear);
+	text_layer_set_text_color(s_time_layer, GColorBlack);
+	text_layer_set_text(s_time_layer, "00:00");
+	
+	// Make it pretty
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 /// Called when the window is unloaded
 static void main_window_unload(Window *window) {
-	
+	text_layer_destroy(s_time_layer);
 }
 
 static void init() {
